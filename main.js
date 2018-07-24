ca731e74e3a6ed671336e3c68403c848f782a058
Eanaj8 <Eanaj8@gmail.com>
2018-07-24 19:40:28 -0400

    new

---
+++ main.js
@@ -0,0 +1,14 @@
+//these are the prompts
+let name = prompt("What is your name");
+let address = propmt("What is your address");
+
+
+
+//these are the console.logs
+console.log(name)
+console.log(address)
+
+//arrays
+
+var fruits = new Array('apples', 'bananas', 'oranges');
+document.write(stuff[0])
